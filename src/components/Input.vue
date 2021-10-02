2b9c1b759148717798d8796dc8a500734b699af9
lproencio <leandroproencio@gmail.com>
2021-10-01 21:12:06 -0300

    init project

---
+++ src/components/Input.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="input">
+    <label for="">{{ label }}</label>
+    <input :type="type" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+  props: {
+    type: String,
+    label: String,
+  },
+});
+</script>
---
+++ src/views/Professional.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <div v-for="(item, i) in inputs_type" :key="i">
+      <Input :type="item.type" :label="item.label" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+import Input from "@/components/Input.vue";
+
+export default defineComponent({
+  name: "Professional",
+  components: { Input },
+  setup() {
+    const inputs_type = ref([
+      { type: "text", label: "Nome Completo*" },
+      { type: "text", label: "CPF*" },
+      { type: "text", label: "Número de celular*" },
+    ]);
+
+    return { inputs_type };
+  },
+});
+</script>
